f0c457cd57bae38f5249caac5621b72f9b63b81f
Jen <bourke.jennifer.0@gmail.com>
2021-05-17 12:59:26 -0400

    Add movement script

---
+++ Donut Game/Assets/Scripts/ConveyorMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorMovement : MonoBehaviour
+{
+    [SerializeField] private Vector3 _velocity;
+
+    private Rigidbody _rigidBody;
+    private Transform _transform;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+        //_rigidBody.velocity = new Vector3(0, 0, -_velocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _transform.position -= _velocity;
+    }
+}
