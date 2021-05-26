25c2b743b5818b3b260067e22b3c1b4dd922791c
Jen <bourke.jennifer.0@gmail.com>
2021-05-26 13:58:56 -0400

    Add scene change

--- Donut Game/Assets/Scripts/WinSpot.cs
+++ Donut Game/Assets/Scripts/WinSpot.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinSpot : MonoBehaviour
 {
     [SerializeField] private ConveyorMovement _conveyorMovement = null;
 
     [SerializeField] private GameObject _youWin = null;
+    [SerializeField] private string _sceneName = "";
+    [SerializeField] private float _sceneTimer = 5f;
+
+    private bool _won = false;
 
     void Awake()
     {
@@ -20,6 +25,19 @@ public class WinSpot : MonoBehaviour
             _conveyorMovement.velocity = Vector3.zero;
             print("dang");
             _youWin.SetActive(true);
+            _won = true;
+        }
+    }
+
+    void Update()
+    {
+        if(_won == true)
+        {
+            _sceneTimer -= Time.deltaTime;
+            if(_sceneTimer <= 0f)
+            {
+                SceneManager.LoadScene(_sceneName);
+            }
         }
     }
 }
