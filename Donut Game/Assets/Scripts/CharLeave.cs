f62fca8f5036f7897e8ad79029a2f02239323a47
Jen <bourke.jennifer.0@gmail.com>
2021-05-28 20:47:57 -0400

    Polish Level 4

--- Donut Game/Assets/Scripts/CharLeave.cs
+++ Donut Game/Assets/Scripts/CharLeave.cs
@@ -21,21 +21,24 @@ public class CharLeave : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter(Collider donut)
     {
-        //gameObject.tag = "notNut";
-
-        if(donut.GetComponent<Renderer>().material.color == _customerMaterial.color && _customerMaterial.color != Color.white && _customerMaterial.color != Color.black)
+        if (donut.GetComponent<Renderer>().material.color == _customerMaterial.color && _customerMaterial.color != Color.white && _customerMaterial.color != Color.black)
         {
             _animator.SetBool("Good", true);
             _customerMaterial.SetColor("_Color", Color.white);
             _holdScore.scoreHolder += 1;
             //print("yayyyyyy");
         }
-        else if(donut.GetComponent<Renderer>().material.color != _customerMaterial.color && _customerMaterial.color != Color.white && _customerMaterial.color != Color.black)
+        else if (donut.GetComponent<Renderer>().material.color != _customerMaterial.color && _customerMaterial.color != Color.white && _customerMaterial.color != Color.black)
         {
             _animator.SetBool("Bad", true);
             _customerMaterial.SetColor("_Color", Color.black);
             _holdScore.scoreHolder -= 1;
             //print("ouchies");
         }
+        //gameObject.tag = "notNut";
+        /*if (donut.tag == "donut")
+        {
+            
+        }*/
     }
 }
